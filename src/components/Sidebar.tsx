64b995d0a51405e56ad407b8ee086bd763740b15
wagner1093 <wagner.1093@gmail.com>
2026-04-10 14:28:47 -0300

    fix: remove unused useNavigate in Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import {
   LayoutDashboard, Wallet, Server, Sparkles, MessageSquare, LogOut,
   TrendingUp, Users, Bell, Calendar, FileText, ChevronRight
